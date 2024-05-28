1694d1b360ecdc4812e9e34d956c31ef6e1ead4c
KRZYSZTOF-PC\Krzysztof Tryc <jaaxx91@hotmail.com>
2024-05-28 22:30:11 +0200

    pass the 2nd scenario but started failing on the 1st one

--- DiamondKata/DiamondKata/DiamondKataService.cs
+++ DiamondKata/DiamondKata/DiamondKataService.cs
@@ -1,10 +1,51 @@
-﻿namespace DiamondKata
+﻿using System.Text;
+
+namespace DiamondKata
 {
     public class DiamondKataService
     {
+        private static char[] Alphabet = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToArray();
+
+        private static char Space = '_';
+
         public string GetDiamondPrint(char letter)
         {
-            return letter.ToString();
+            var indexOfMidpointLetter = Array.IndexOf(Alphabet, letter);
+
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i <= indexOfMidpointLetter; i++)
+            {
+                stringBuilder.Append(new string(Space, indexOfMidpointLetter - i));
+
+                if (i == 0)
+                {
+                    stringBuilder.Append(Alphabet[i]);
+                }
+                else
+                {
+                    stringBuilder.Append(Alphabet[i]);
+                    stringBuilder.Append(new string(Space, indexOfMidpointLetter));
+                    stringBuilder.Append(Alphabet[i]);
+                }
+
+                stringBuilder.Append(new string(Space, indexOfMidpointLetter - i));
+                stringBuilder.AppendLine();
+            }
+
+            for (int i = indexOfMidpointLetter; i > 0; i--)
+            {
+                stringBuilder.Append(new string(Space, i));
+                stringBuilder.Append(Alphabet[i-1]);
+                stringBuilder.Append(new string(Space, i));
+
+                if (i-1 != 0)
+                {
+                    stringBuilder.AppendLine();
+                }
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
